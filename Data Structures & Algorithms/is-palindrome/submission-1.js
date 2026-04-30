190208a3848b59bb7c362ef9c08988ea86b8e993
hamzaachana-crypto <hamzaachana@gmail.com>
2026-04-30 15:57:35 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(str) {
+    const cleaned = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+  let pointer1 = 0;
+  let pointer2 = cleaned.length - 1;
+
+  
+
+  while (pointer1 < pointer2) {
+    if (cleaned[pointer1] !== cleaned[pointer2]) return false;
+
+    pointer1++;
+    pointer2--;
+  }
+
+  return true;
+}
+    }
+
