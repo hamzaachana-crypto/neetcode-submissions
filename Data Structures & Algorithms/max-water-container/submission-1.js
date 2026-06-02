3cab5a89e05e33c8c832488cded8ff801e916727
hamzaachana-crypto <hamzaachana@gmail.com>
2026-06-02 15:29:47 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+      let left = 0;
+  let right = heights.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    const width = right - left;
+    const height = Math.min(heights[left], heights[right]);
+    const area = height * width;
+
+    console.log('left,right', left, right);
+
+    if (heights[left] < heights[right]) {
+      max = Math.max(max, area);
+      left++;
+    } else if (heights[left] > heights[right]) {
+      max = Math.max(max, area);
+      right--;
+    } else {
+      max = Math.max(max, area);
+      left++;
+      right--;
+    }
+  }
+  return max;
+    }
+}
