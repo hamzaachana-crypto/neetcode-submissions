2e0ad2c90815d9347a60e5628cddc652740e4f3e
hamzaachana-crypto <hamzaachana@gmail.com>
2026-06-04 16:09:45 +0100

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+         const hash = {};
+  for (let j = 0; j < nums.length; j++) {
+    hash[nums[j]] = j;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let difference = target - nums[i];
+
+    if (difference in hash && i !== hash[difference]) {
+      return [i, hash[difference]];
+    }
+  }
+  return [];
+    }
+}
