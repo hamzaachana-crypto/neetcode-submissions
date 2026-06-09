490928d00a7b0cb9023c918bab28b92808462655
hamzaachana-crypto <hamzaachana@gmail.com>
2026-06-09 17:54:25 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const groups = {};
+
+  for (let str of strs) {
+    const count = new Array(26).fill(0);
+
+    for (let char of str) {
+      count[char.charCodeAt(0) - 97]++;
+    }
+
+
+    const key = count.join('#');
+
+
+    if (!groups[key]) {
+      groups[key] = [];
+    }
+
+    groups[key].push(str);
+  }
+
+
+   return Object.values(groups);
+    }
+}
